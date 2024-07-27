d286489035e097d73164a44ce2503ac275f5412d
shoikotsen <shoikot@usf.edu>
2024-07-27 22:44:35 +0000

    comments

--- calculate.cs
+++ calculate.cs
@@ -2,21 +2,29 @@ using System;
 
 public class Calculate
 {
+    // Method to compare the number of contestants from the previous year and the current year
     public void CompareContestants(int previousYearContestants, int currentYearContestants)
     {
+        // Display the number of contestants for both years
         Console.WriteLine($"Last year's competition had {previousYearContestants} contestants, " +
                           $"and this year's has {currentYearContestants} contestants.");
-
+        // Check if the number of contestants this year is more than twice the number from last year
         if (currentYearContestants > previousYearContestants * 2)
         {
+            // Display a message indicating the competition has more than doubled
             Console.WriteLine("The competition is more than twice as big this year!");
         }
+        // Check if the number of contestants this year is greater than last year
         else if (currentYearContestants > previousYearContestants)
         {
+            // Display a message indicating the competition is bigger than ever
+
             Console.WriteLine("The competition is bigger than ever!");
         }
+        // If the number of contestants this year is less than or equal to last year
         else
         {
+            // Display a message indicating a closer competition
             Console.WriteLine("A tighter race this year! Come out and cast your vote!");
         }
     }
--- main.cs
+++ main.cs
@@ -2,45 +2,59 @@ using System;
 
 class Program
 {
+    // Main entry point of the program
     static void Main(string[] args)
     {
+        // Create an instance of the Calculate class
         Calculate calculate = new Calculate();
-
+        // Display the program's motto at the start
         DisplayMotto();
+        // Start an infinite loop to display the menu and handle user choices
         while (true)
         {
+            
+            // Display the menu options to the user
             DisplayMenu();
+            // Read the user's choice from the console input
             string choice = Console.ReadLine();
+            // Check the user's choice and perform the corresponding action
             if (choice == "1")
             {
+                // Prompt the user to enter the number of contestants from the previous year
                 Console.Write("Enter the number of contestants from the previous year: ");
                 int previousYearContestants = int.Parse(Console.ReadLine());
-
+                // Prompt the user to enter the number of contestants for the current year
                 Console.Write("Enter the number of contestants for the current year: ");
                 int currentYearContestants = int.Parse(Console.ReadLine());
 
+                // Compare the number of contestants and display the results
+                
                 calculate.CompareContestants(previousYearContestants, currentYearContestants);
+                // Display the motto again after the comparison
                 DisplayMotto();
             }
             else if (choice == "2")
             {
+                // If the user chooses to exit, display a goodbye message and break the loop
                 Console.WriteLine("Thank you for using the Greenville Revenue App, good-bye!");
                 break;
             }
             else
             {
+                // If the user enters an invalid choice, prompt them to select again
                 Console.WriteLine("Invalid choice, please select again.");
             }
         }
     }
-
+    // Method to display the program's motto
     static void DisplayMotto()
     {
         Console.WriteLine("*********************************");
         Console.WriteLine("* The stars shine in Greenville. *");
         Console.WriteLine("*********************************");
     }
-
+    
+    // Method to display the menu options to the user
     static void DisplayMenu()
     {
         Console.WriteLine("\nPlease Enter the following number below from the following menu:");
